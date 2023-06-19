67b14a5c9bf7e776c03e7fc6796e87012b52e1f5
Neemias Almeida <neemias.junior@gmail.com>
2023-06-19 23:38:06 +0200

    Add push and pop on linked list

--- linked-list/src/first.rs
+++ linked-list/src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 // Bad Singly-Linked Stack: http://rust-unofficial.github.io/too-many-lists/first.html
 #[allow(dead_code)]
 pub struct List {
@@ -7,6 +9,24 @@ pub struct List {
 impl List {
     pub fn new() -> Self {
         Self { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node)
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
     }
 }
 
@@ -19,6 +39,5 @@ enum Link {
 #[allow(dead_code)]
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
-
